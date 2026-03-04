58801949e59291bda54240fa80bd77d8651633ea
darapqqq <pasenkodara88@gmail.com>
2026-03-04 09:26:40 +0500

    удаление пользователя

--- p13.py
+++ p13.py
@@ -17,6 +17,7 @@ cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)',
                ('newuser', 'newuser@example.com', 28))
 
 cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
+cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser',))
 
 connection.commit()
 connection.close()
