f5a98f506345af3f9e767c92abeaa1beb84bb300
darapqqq <pasenkodara88@gmail.com>
2026-03-04 09:28:48 +0500

    select

--- p13.py
+++ p13.py
@@ -15,9 +15,17 @@ cursor.execute('''
 
 cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', 
                ('newuser', 'newuser@example.com', 28))
+cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', 
+               ('user1', 'newuser1@example.com', 25))
 
 cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
 cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser',))
 
+cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
+
+for user in users:
+    print(user)
+    
 connection.commit()
 connection.close()
