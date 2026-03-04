4a594f3c3acb7e35ccb0b49e2cb6cb06d972f492
darapqqq <pasenkodara88@gmail.com>
2026-03-04 09:46:00 +0500

    управление данными

--- p13.py
+++ p13.py
@@ -1,35 +1,17 @@
 import sqlite3
 
-
-connection = sqlite3.connect('my_database.db')
-cursor = connection.cursor()
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Users (
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL,
-        age INTEGER
-    )
-''')
-
-cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', 
-               ('newuser', 'newuser@example.com', 28))
-cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', 
-               ('user1', 'newuser1@example.com', 25))
-
-try:
-    cursor.execute('BEGIN')
+with sqlite3.connect('my_database.db') as connection:
+    cursor = connection.cursor()
     
-    cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', 
-                   ('user1', 'user1@example.com'))
-    cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', 
-                   ('user2', 'user2@example.com'))
-    
-    cursor.execute('COMMIT')
-    
-except:
-    cursor.execute('ROLLBACK')
+    try:
+        with connection:
+            cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', 
+                          ('user3', 'user3@example.com'))
+            cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', 
+                          ('user4', 'user4@example.com'))
+            
+    except:
+        pass
     
 connection.commit()
 connection.close()
