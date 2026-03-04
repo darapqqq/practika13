acde6d8a0fb41e803b385f10d9a706bb75c84e30
darapqqq <pasenkodara88@gmail.com>
2026-03-04 09:37:31 +0500

    oldest_users

--- p13.py
+++ p13.py
@@ -18,28 +18,15 @@ cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)',
 cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', 
                ('user1', 'newuser1@example.com', 25))
 
-cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
-cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser',))
+cursor.execute('''
+    SELECT username, age 
+    FROM Users 
+    WHERE age = (SELECT MAX(age) FROM Users)
+''')
+oldest_users = cursor.fetchall()
 
-cursor.execute('SELECT COUNT(*) FROM Users')
-total_users = cursor.fetchone()[0]
-print('Общее количество пользователей:', total_users)
-
-cursor.execute('SELECT SUM(age) FROM Users')
-total_age = cursor.fetchone()[0]
-print('Общая сумма возрастов пользователей:', total_age)
-
-cursor.execute('SELECT AVG(age) FROM Users')
-average_age = cursor.fetchone()[0]
-print('Средний возраст пользователей:', average_age)
-
-cursor.execute('SELECT MIN(age) FROM Users')
-min_age = cursor.fetchone()[0]
-print('Минимальный возраст среди пользователей:', min_age)
-
-cursor.execute('SELECT MAX(age) FROM Users')
-max_age = cursor.fetchone()[0]
-print('Максимальный возраст среди пользователей:', max_age)
+for user in oldest_users:
+    print(user)
     
 connection.commit()
 connection.close()
