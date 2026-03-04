3d279e0efedaaee335c3ec1caa1566a3e56377a4
darapqqq <pasenkodara88@gmail.com>
2026-03-04 09:36:03 +0500

    методичка 4c

--- p13.py
+++ p13.py
@@ -21,17 +21,25 @@ cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)',
 cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
 cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser',))
 
-cursor.execute('''
-    SELECT username, age, AVG(age) 
-    FROM Users 
-    GROUP BY age 
-    HAVING AVG(age) > ? 
-    ORDER BY age DESC
-''', (30,))
-results = cursor.fetchall()
+cursor.execute('SELECT COUNT(*) FROM Users')
+total_users = cursor.fetchone()[0]
+print('Общее количество пользователей:', total_users)
 
-for row in results:
-    print(row)
+cursor.execute('SELECT SUM(age) FROM Users')
+total_age = cursor.fetchone()[0]
+print('Общая сумма возрастов пользователей:', total_age)
+
+cursor.execute('SELECT AVG(age) FROM Users')
+average_age = cursor.fetchone()[0]
+print('Средний возраст пользователей:', average_age)
+
+cursor.execute('SELECT MIN(age) FROM Users')
+min_age = cursor.fetchone()[0]
+print('Минимальный возраст среди пользователей:', min_age)
+
+cursor.execute('SELECT MAX(age) FROM Users')
+max_age = cursor.fetchone()[0]
+print('Максимальный возраст среди пользователей:', max_age)
     
 connection.commit()
 connection.close()
