bc43423108abc420d641269c7439d97afa3114af
darapqqq <pasenkodara88@gmail.com>
2026-03-04 09:41:55 +0500

    5а

--- p13.py
+++ p13.py
@@ -19,10 +19,16 @@ cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)',
                ('user1', 'newuser1@example.com', 25))
 
 cursor.execute('SELECT * FROM Users')
-users = cursor.fetchall()
+first_user = cursor.fetchone()
+print(first_user)
 
-for user in users:
-    print(user)
-    
+cursor.execute('SELECT * FROM Users')
+first_five_users = cursor.fetchmany(5)
+print(first_five_users)
+
+cursor.execute('SELECT * FROM Users')
+all_users = cursor.fetchall()
+print(all_users)
+
 connection.commit()
 connection.close()
